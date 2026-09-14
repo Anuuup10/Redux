901dc7318b4fc1f13b72360b7bd951b6bd9b6a70
Anup Chaudhary <74030059+Anup10430@users.noreply.github.com>
2026-09-14 19:08:33 +0545

    Feat: Creating Reducer & Action Creator in Redux

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,11 +3,11 @@ import React from "react";
 const Shop = () => {
   return (
     <div>
-        <h2>Buy Addidas Shoes 50 Rs</h2>
+        <h2>Deposit/Withdraw Money</h2>
       <button disabled={true} className="btn btn-primary mx-2">
         -
       </button>
-      Add to cart
+      Update Balance
       <button disabled={true} className="btn btn-primary mx-2">
         +
       </button>
